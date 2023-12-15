64ef2401b4280e65991662f124c24ca4b130fe9c
peregrinewolf <mrosario5664@gmail.com>
2023-12-14 20:18:50 -0600

    first commit

--- website blocker/website_blocker.py
+++ website blocker/website_blocker.py
@@ -1,27 +1,33 @@
 from datetime import datetime
 import pyuac
 
-endTime = datetime(2024,10,10,20)
+endTime = datetime()
 
 # type the website to block
-site_to_block = ['www.smashkarts.io','smashkarts.io' ]
+#site_to_block = ['www.WebsiteName','WebsiteName' ]
+site_to_block = []
 
-#hostPathM= r"/etc/host"
+#for mac
+hostPathM= r"/etc/host"
+
+#for Windows
 hostPathW= r"C:\Windows\System32\drivers\etc\hosts"
+
+hostPath = hostPathW
 
 redirect = "127.0.0.1"
 
 def block_sites():
     if datetime.now() < endTime:
         print("block sites")
-        with open(hostPathW, 'r+') as hostsfile:
+        with open(hostPath, 'r+') as hostsfile:
             host_content= hostsfile.read()
             for site in site_to_block:
                 if site not in host_content:
                     hostsfile.write(redirect + " " + site + "\n")
     else:
         print("unblock sites") 
-        with open(hostPathW, 'r+') as hostsfile:
+        with open(hostPath, 'r+') as hostsfile:
             lines = hostsfile.readlines()
             hostsfile.seek(0)
             for line in lines:
@@ -34,8 +40,6 @@ if __name__ == "__main__":
         print("Re-launching as admin!")
         pyuac.runAsAdmin()
     
-    #1. run manually
-    #2. cron job
-    #
+   
     else:
         block_sites()
